d59293e7c047f4c608705a588a7028114a46ad84
bruno montebello <brunormontebello@hotmail.com>
2020-04-26 13:09:43 -0300

    Add edit, update and delete article actions + update the views with links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,12 +19,23 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+    end
   end
 
   private
