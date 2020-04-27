163d3ecba0861df89998b18cce81e45ad139c0b3
bruno montebello <brunormontebello@hotmail.com>
2020-04-27 17:43:33 -0300

    Add errors checking for Articles new form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
-            length: {minimu: 3}
+            length: {minimum: 3}
+
+  validates :text, presence: true,
+            length: {minimum: 2}
 end
